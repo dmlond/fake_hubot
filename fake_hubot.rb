8984cd75d4c5c137bc679006b0561736c37db670
Darin London <darin.london@duke.edu>
2018-05-25 14:13:49 -0400

    added liveness and readyness probes

--- fake_hubot.rb
+++ fake_hubot.rb
@@ -6,6 +6,18 @@ require 'sinatra/reloader' if development?
 
 set :show_exceptions, false
 
+configure :development do
+  set :logging, Logger::DEBUG
+end
+
+get '/liveness' do
+ 'alive'
+end
+
+get '/readyness' do
+  'ready'
+end
+
 get '/hubot' do
   "hello from hubot"
 end
